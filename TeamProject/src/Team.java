fcddd6d584f9049b0dd3a7603546588b837d3539
Java1-2012024027 <jgl2002@naver.com>
2016-06-19 23:27:24 +0900

    hot

--- TeamProject/src/Team.java
+++ TeamProject/src/Team.java
@@ -1,4 +1,55 @@
 
-public class Team {
+	import java.awt.event.ActionEvent;
+	public class Team {	
+		public void actionPerformed(ActionEvent e) {
+			String command = e.getActionCommand();
+			if (command.charAt(0) == 'C') {
+				startOfNumber = true;
+				result = 0;
+				operator = "=";
+				display.setText("0.0");
+			} else if (command.charAt(0) >= '0' && command.charAt(0) <= '9' || command.equals(".")) {
+				if (startOfNumber == true)
+					display.setText(command);
+				else
+					display.setText(display.getText() + command);
+				startOfNumber = false;
+			} else {
+				if (startOfNumber) {
+					if (command.equals("-")) {
+						display.setText(command);
+						startOfNumber = false;
+					} else
+						operator = command;
+				} else {
+					double x = Double.parseDouble(display.getText());
+					calculate(x);
+					operator = command;
+					startOfNumber = true;
+				}
+			}
+		}
+
+		private void calculate(double n) {
+			if (operator.equals("+"))
+				result += n;
+			else if (operator.equals("-"))
+				result -= n;
+			else if (operator.equals("*"))
+				result *= n;
+			else if (operator.equals("/"))
+				result /= n;
+			else if (operator.equals("="))
+				result = n;
+			display.setText(" " + result);
+		}
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			Calculator s = new Calculator();
+	
+		}
+
+	}
 
 }
